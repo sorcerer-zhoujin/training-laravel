9c0b797bc8ecbab36230e17ad534eb3cfbff08c0
Zhou Jin <zhoujin@sorcerer.co.jp>
2023-04-10 17:59:52 +0900

    ガチャ関数完成しました

--- app/Http/Controllers/PlayerItemController.php
+++ app/Http/Controllers/PlayerItemController.php
@@ -12,7 +12,7 @@ class PlayerItemController extends Controller
 {
     public function add(Request $request, $id)
     {
-        $target = PlayerItem::query()->where(['player_id' => $id, 'item_id' => $request->input('itemId')]);
+        $target = PlayerItem::query()->where(['player_id' => $id, 'itemId' => $request->input('itemId')]);
         $num = $request->input('count');
         // プレーヤーは既にアイテムも持っている場合（加算）
         if ($target->exists()) {
@@ -23,7 +23,7 @@ class PlayerItemController extends Controller
         else {
             PlayerItem::insertGetId([
                 'player_id' => $id,
-                'item_id' => $request->input('itemId'),
+                'itemId' => $request->input('itemId'),
                 'count' => $num
             ]);
         }
@@ -44,7 +44,7 @@ class PlayerItemController extends Controller
         $MAX_MP = 200;
 
         // データ
-        $target = PlayerItem::query()->where(['player_id' => $id, 'item_id' => $request->input('itemId')]);
+        $target = PlayerItem::query()->where(['player_id' => $id, 'itemId' => $request->input('itemId')]);
         // プレーヤー情報
         $player = Player::query()->where('id', $id);
         $playerHp = $player->value('hp');
@@ -55,7 +55,7 @@ class PlayerItemController extends Controller
             return new Response('アイテムなし', $ERR_CODE);
         }
         // アイテム情報
-        $itemValue = Item::query()->where('id', $target->value('item_id'))->value('value');
+        $itemValue = Item::query()->where('id', $target->value('itemId'))->value('value');
         $itemCount = $target->value('count');
         if ($itemCount < $request->input('count'))
         {
@@ -69,7 +69,7 @@ class PlayerItemController extends Controller
         }
 
         // HP回復
-        if ($target->value('item_id') == 1) {
+        if ($target->value('itemId') == 1) {
             for ($i = $request->input('count'); $i > 0; $i--) {
                 $playerHp = ($playerHp + $itemValue) < $MAX_HP ? ($playerHp + $itemValue) : $MAX_HP;
                 $itemCount--;
@@ -77,7 +77,7 @@ class PlayerItemController extends Controller
             }
         }
         // MP回復
-        if ($target->value('item_id') == 2) {
+        if ($target->value('itemId') == 2) {
             for ($i = $request->input('count'); $i > 0; $i--) {
                 $playerMp = ($playerMp + $itemValue) < $MAX_MP ? ($playerMp + $itemValue) : $MAX_MP;
                 $itemCount--;
@@ -92,7 +92,7 @@ class PlayerItemController extends Controller
 
         // レスポンス
         return new Response([
-            'itemId' => $target->value("item_id"),
+            'itemId' => $target->value("itemId"),
             'count' => $itemCount,
             'player' => [
                 'id' => $player->value('id'),
@@ -100,5 +100,119 @@ class PlayerItemController extends Controller
                 'mp' => $playerMp
             ]
         ]);
+    }
+
+    public function gacha(Request $request, $id)
+    {
+        $COUNT = $request->input('count');     // ガチャ回数
+        $COST = 10;                            // ガチャ一回の費用
+        $gachaCost = $COST * $COUNT;           // ガチャ合計費用
+
+        // プレーヤー情報
+        $player = Player::query()->where('id', $id);
+        $playerMoney = $player->value('money');
+        // 所持金の判断
+        if ($player->value('money') < $gachaCost) {
+            return new Response('ガチャ費用不足');
+        }
+        // お金かかる
+        else {
+            $playerMoney -= $gachaCost;
+            $player->update([
+                'money' => $playerMoney
+            ]);
+        }
+
+        // すべてのアイテムのデータ
+        $itemPool = Item::get();
+        // アイテムデータから確率を取得して配列に保存
+        $lootPercent = [];
+        foreach ($itemPool as $item) {
+            $lootPercent[$item->id] = $item->percent;
+        }
+
+        // ガチャ結果用の配列
+        $result = $this->lottery($lootPercent, $COUNT);
+        // アウトプット用の配列
+        $resultItemCounter = array_fill(0, $itemPool->count() + 1, 0);
+        if (empty($result)) return;
+
+        // プレーヤーのアイテム情報を配列に保存
+        $playerItems = PlayerItem::where('player_id', $id)
+            ->get()
+            ->keyBy('item_id')
+            ->map(function ($item) {
+                return [
+                    'itemId' => $item->item_id,
+                    'count' => $item->count
+                ];
+            })
+            ->toArray();
+        
+        // 結果によるデータを更新
+        foreach ($result as $itemId) {
+            $resultItemCounter[$itemId]++;
+            // ハズレ
+            if ($itemId == 0) continue;
+            //　プレーヤーのアイテム情報は既にテーブルに存在する場合、更新のみ
+            if (isset($playerItems[$itemId])) {
+                $playerItems[$itemId]['count']++;
+                PlayerItem::query()
+                    ->where('player_id', $id)
+                    ->where('item_id', $itemId)
+                    ->update(['count' => $playerItems[$itemId]['count']]);
+            }
+            //　プレーヤーのアイテム情報は存在しない、新規作成
+            else {
+                PlayerItem::query()
+                    ->insert([
+                    'player_id' => $id,
+                    'item_id' => $itemId,
+                    'count' => 1
+                ]);
+                $playerItems[$itemId] = ['count' => 1];
+            }
+        }
+        
+        // レスポンス
+        return new Request([
+            'results' => [
+                'itemId' => array_keys($resultItemCounter),
+                'count' => array_values($resultItemCounter)
+            ],
+            'player' => [
+                'money' => $playerMoney,
+                'items' => $playerItems
+            ]
+        ]); 
+    }
+
+    // ガチャ結果計算用の関数
+    private function lottery($lootPercent, $numTimes) {
+        $MAX_PERCENT = 100;
+        $totalPercent = array_sum($lootPercent);
+    
+        if ($totalPercent < $MAX_PERCENT) {
+            $lootPercent[0] = $MAX_PERCENT - $totalPercent;
+            $totalPercent = $MAX_PERCENT;
+        }
+
+        $result = array();
+
+        for ($i = 0; $i < $numTimes; $i++) {
+            $random = rand(1, $totalPercent);
+            $currentPercent = 0;
+    
+            foreach ($lootPercent as $id => $percent) {
+                $currentPercent += $percent;
+    
+                if ($random <= $currentPercent) {
+                    $result[] = $id;
+                    break;
+                }
+            }
+        }
+    
+        return $result;
     }
 }
