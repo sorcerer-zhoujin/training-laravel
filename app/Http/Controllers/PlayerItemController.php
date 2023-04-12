74246e44564cf7b69902d305d0b795ce9cdcf322
Zhou Jin <zhoujin@sorcerer.co.jp>
2023-04-12 16:25:41 +0900

    add()関数にテーブルロックを追加

--- app/Http/Controllers/PlayerItemController.php
+++ app/Http/Controllers/PlayerItemController.php
@@ -19,7 +19,7 @@ class PlayerItemController extends Controller
         DB::beginTransaction();
 
         try {
-            $target = PlayerItem::query()->where(['player_id' => $id, 'item_id' => $request->input('itemId')]);
+            $target = PlayerItem::query()->where(['player_id' => $id, 'item_id' => $request->input('itemId')])->lockForUpdate();
             $num = $request->input('count');
             // プレーヤーは既にアイテムも持っている場合（加算）
             if ($target->exists()) {
